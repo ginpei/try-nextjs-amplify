7ccd10f8cc20d27ff5bd1d9bc49466366184d123
Ginpei <ginpei@ginpei.dev>
2021-03-07 16:00:49 -0800

    Initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import { AppProps } from "next/app";
+import { ReactElement } from "react";
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }: AppProps): ReactElement {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
